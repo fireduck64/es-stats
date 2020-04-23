fd337a5475d26116b06cd479ebea582592964cdf
fireduck64 <fireduck@gmail.com>
2020-04-23 13:18:32 -0700

    Add limit to nonsense

--- dht22/sensor.py
+++ dht22/sensor.py
@@ -21,7 +21,7 @@ report = {
   "humidity": round(humidity,3),
   "temperature": round(temperature,3)
 }
-if (humidity > 105.0) or (temperature > 100):
+if (humidity > 105.0) or (temperature > 100.0) or (temperature < -70.0) or (humidity < 0.0):
   report = { "error": "nonsense values" }
 
 
