5793ec0e10595f28cc419cbe932bfe6925b9a14c
fireduck64 <fireduck@gmail.com>
2020-04-23 09:49:09 -0700

    better json

--- dht22/sensor.py
+++ dht22/sensor.py
@@ -3,6 +3,8 @@
 import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
+import sys
+import json
 
 # apt-get install python3-pip
 # pip3 install RPi.GPIO Adafruit_DHT
@@ -15,9 +17,11 @@ GPIO.setmode(GPIO.BCM)
 DHT_SENSOR = Adafruit_DHT.DHT22
 
 humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-if humidity is not None and temperature is not None:
-  print('{"temp":{0:0.1f},"humidity":{1:0.1f}%}'.format(temperature, humidity))
-else:
-  print('{"error":"Failed to retrieve data from humidity sensor")')
+report = { 
+  "humidity": round(humidity,3),
+  "temperature": round(temperature,3)
+}
+
+print(json.dumps(report));
 
 
