e93d3345799d6a7e8971c8f03b4bf164aedc44e7
fireduck64 <fireduck@gmail.com>
2020-04-23 09:26:34 -0700

    switch to command line single mode and output json

--- dht22/sensor.py
+++ dht22/sensor.py
@@ -7,20 +7,17 @@ import time
 # apt-get install python3-pip
 # pip3 install RPi.GPIO Adafruit_DHT
 
-DHT_PIN = 4
+DHT_PIN = int(sys.argv[1])
 GPIO.setmode(GPIO.BCM)
 
 #GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 DHT_SENSOR = Adafruit_DHT.DHT22
 
-
-while True:
-  humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-  if humidity is not None and temperature is not None:
-  	print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temperature, humidity))
-  else:
-    print("Failed to retrieve data from humidity sensor")
-  time.sleep(2);
+humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+if humidity is not None and temperature is not None:
+  print('{"temp":{0:0.1f},"humidity":{1:0.1f}%}'.format(temperature, humidity))
+else:
+  print('{"error":"Failed to retrieve data from humidity sensor")')
 
 
