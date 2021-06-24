36a3ec2d9e0cbfb7bd33f4538969c706974a89d2
fireduck64 <fireduck@gmail.com>
2021-06-24 09:57:16 -0700

    Update dht22 lib

--- dht22/sensor.py
+++ dht22/sensor.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 
-import Adafruit_DHT
-import RPi.GPIO as GPIO
 import time
 import sys
 import json
+import adafruit_dht
 
-# apt-get install python3-pip
-# pip3 install RPi.GPIO Adafruit_DHT
+# apt-get install python3-pip libgpiod-dev
+# pip3 install adafruit-circuitpython-dht
 
-DHT_PIN = int(sys.argv[1])
-GPIO.setmode(GPIO.BCM)
+pin = int(sys.argv[1])
 
-#GPIO.setup(DHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+dht_device = adafruit_dht.DHT22(pin)
 
-DHT_SENSOR = Adafruit_DHT.DHT22
+temperature = dht_device.temperature
+humidity = dht_device.humidity
 
-humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
 report = { 
   "humidity": round(humidity,3),
   "temperature": round(temperature,3)
 }
 if (humidity > 105.0) or (temperature > 100.0) or (temperature < -70.0) or (humidity < 0.0):
   report = { "error": "nonsense values" }
-
 
 print(json.dumps(report));
 
