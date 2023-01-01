a682b94c46d6ca4280bc9003b2cbea716fc0c62e
fireduck64 <fireduck@gmail.com>
2023-01-01 14:04:49 -0800

    smart exit code

--- smart-mon-count.sh
+++ smart-mon-count.sh
@@ -6,12 +6,13 @@ passed=0
 
 for dev in $(smart-mon-list.sh)
 do
-  status=$(smartctl -j -a $dev|jq .smart_status.passed)
-  if [ "$status" == "true" ]
+  #status=$(smartctl -j -a $dev|jq .smart_status.passed)
+  status=$(smartctl -a /dev/sdb -j|jq .smartctl.exit_status)
+  if [ "$status" == "0" ]
   then
     passed=$((passed+1))
   fi
-  echo "$dev - $(smartctl -j -a $dev|jq .smart_status)"
+  echo "$dev - $status"
 done
 
 echo "{}" | addtag.py passed $passed host $HOSTNAME | addtime.py | sendjson.sh smartmon
